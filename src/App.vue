563f94ee2728a677885eec8cd020f06498e3fbee
Amber <amber20240@gmail.com>
2020-01-08 10:54:34 +0800

    todolist-vue-20200108

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <todo-input @emit2="emit3"></todo-input>
+    <ul>
+    <todo-item v-for="(item,index) in text2" :item="item" @click.native="remove(index)"></todo-item>
+    </ul>
+  </div>
+</template>
+<script>
+import todoInput from "./todoInput.vue"
+import todoItem from "./todoItem.vue"
+export default {
+    data(){
+      return {
+        text2:[],
+      }
+    },
+    components:{
+      todoInput,
+      todoItem
+    },
+    methods:{
+      emit3(addtodo){
+        this.text2.push(addtodo)
+      },
+      remove(index){
+        this.text2.splice(index,1)
+      }
+    }
+}
+</script>
---
+++ src/todoInput.vue
@@ -0,0 +1,24 @@
+<template>
+<div @click="addemit" @keydown.enter="addemit">
+    <input type="text" v-model="addtodo">
+    <button type="button">submit</button>
+</div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            addtodo:""
+        }
+    },
+    methods:{
+        addemit(){
+            if(this.addtodo == ""){
+                return
+            };
+            this.$emit("emit2",this.addtodo);
+            this.addtodo=""
+        }
+    }
+ }
+</script>
